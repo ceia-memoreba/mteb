82525c34ca32a270dfd4f736b45faca7484dd08f
amandaabp <58114420+amandaabp@users.noreply.github.com>
2024-03-01 06:26:12 -0300

    Incluindo task com dataset brt

---
+++ scripts/run_mteb_pt.py
@@ -0,0 +1,25 @@
+"""Example script for benchmarking all datasets constituting the MTEB English leaderboard & average scores"""
+
+import logging
+
+from mteb import MTEB
+from sentence_transformers import SentenceTransformer
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger("main")
+
+classification_tasks = [
+    "AmazonPolarityClassification"
+]
+
+task = classification_tasks
+
+model_name = "intfloat/multilingual-e5-small"
+model = SentenceTransformer(model_name, device='cpu')
+
+#for task in TASK_LIST:
+logger.info(f"Running task: {task}")
+eval_splits = ["dev"] if task == "MSMARCO" else ["test"]
+evaluation = MTEB(tasks=[task], task_langs=["pt"])  # Remove "en" for running all languages
+evaluation.run(model, output_folder=f"results/{model_name}", eval_splits=eval_splits)
