b472986769409976750dbb5a4b55025683899b64
amandaabp <58114420+amandaabp@users.noreply.github.com>
2024-03-08 06:56:08 -0300

    moving to folder

--- mteb/tasks/Classification/portuguese/BrazilianCourtDecisionsClassification.py
+++ mteb/tasks/Classification/portuguese/BrazilianCourtDecisionsClassification.py
@@ -1,4 +1,6 @@
-from ...abstasks import AbsTaskClassification
+from mteb import MTEB
+from mteb.abstasks.AbsTaskClassification import AbsTaskClassification
+from sentence_transformers import SentenceTransformer
 
 
 class BrazilianCourtDecisionsClassification(AbsTaskClassification):
@@ -9,10 +11,14 @@ class BrazilianCourtDecisionsClassification(AbsTaskClassification):
             "hf_hub_name": "projetomemoreba/mteb_brazilian_court_decisions",
             "description": "Amazon Polarity Classification Dataset.",
             "reference": "https://dl.acm.org/doi/10.1145/2507157.2507163",
-            "category": "p2p",
+            "category": "s2s",
             "type": "Classification",
-            "eval_splits": ["test"],
-            "eval_langs": ["en"],
+            "eval_splits": ["validation", "test"],
+            "eval_langs": ["pt"],
             "main_score": "accuracy",
             "revision": "ae5cdd58be9e246773486042e743abc6832906c8",
         }
+
+model = SentenceTransformer("average_word_embeddings_komninos")
+evaluation = MTEB(tasks=[BrazilianCourtDecisionsClassification()])
+evaluation.run(model)
